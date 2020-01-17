bd1b7326b87c7f467ee62e2821c5490502feb419
Tester <tester@MBP-17-2554.local>
2020-01-17 11:14:29 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HerokuTest',
+    version='1.0.0',
+    packages=[''],
+    url='localhost',
+    license='',
+    author='iTexico',
+    author_email='yin9517@gmail.com',
+    description='My first heroku test'
+)
